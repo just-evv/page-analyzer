e4bea172b45e8b8da1c1bfeae15f6b264e37a3b9
Ev Sivkova <ev.sivkova@outlook.com>
2021-11-08 15:57:39 -0800

    smth

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,53 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
     <title>Analyzator</title>
 </head>
 
-<body>
-<header class="container-fluid">
-    <nav>
-        <ul>
-            <li><a href="/">Analyzator</a></li>
-            <li><a href="/">Main</a></li>
-            <li><a href="/urls">Sites</a></li>
-        </ul>
+<body class="min-vh-100 d-flex flex-column">
+
+<header class="flex-shrink-0">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Analyzer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="/">Main</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="/urls">Added</a>
+                </li>
+            </ul>
+        </div>
     </nav>
 </header>
-<h1 class="text-center">Analyzer</h1>
+
+@if ($errors->any())
+    <div class="alert alert-danger container-fluid" role="alert">
+        @include('flash::message')
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+
+    </div>
+@endif
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-<div class="container">
-    <h2 class="text-center">Check url</h2>
+<div class="container-expand-md bg-dark text-white align-items-center">
 
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            @include('flash::message')
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+    <h1 class="display-1 text-center" >Analyzer</h1>
 
-    <form action="/" method="post">
+    <h2 class="display-7 text-center">Check url for seo-readiness</h2>
+
+    <form class="form-inline" action="/" method="post" >
+
         @csrf <!-- {{ csrf_field() }} -->
-        <div>
             <label>
-                url
-                <input type="text" required name="name" >
+                <input class="form-control form-control-lg m-2" type="text" required name="name" placeholder="http://example.com">
             </label>
-        </div>
-        <input type="submit" value="Check">
-    </form>
+            <button type="submit" class="btn btn-primary btn-lg m-2">Check url </button>
+        </form>
+
+
 </div>
+
+<footer class="border-top py-3 mt-5 flex-shrink-0">
+    <div class="container-lg">
+        <div class="text-center">
+            <a>Analyzer</a>
+        </div>
+    </div>
+</footer>
 <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
 </body>
