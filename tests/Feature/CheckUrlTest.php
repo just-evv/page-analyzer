26d755f5e36659640e17222d705736e4ae4f09bd
Ev Sivkova <ev.sivkova@outlook.com>
2021-12-08 16:17:16 -0800

    fx path

--- tests/Feature/CheckUrlTest.php
+++ tests/Feature/CheckUrlTest.php
@@ -12,6 +12,12 @@ class CheckUrlTest extends TestCase
 {
     use DatabaseMigrations;
     use DatabaseTransactions;
+    
+    public function getPathFixture(string $fixtureName): string
+    {
+        return realpath(implode(DIRECTORY_SEPARATOR, [__DIR__, '..', 'Fixtures', $fixtureName]));
+    }
+
 
     public function testCreateCheck(): void
     {
@@ -19,8 +25,9 @@ class CheckUrlTest extends TestCase
         $id = DB::table('urls')->insertGetId([
             'name' => $domainName,
         ]);
-
-        $testPage = file_get_contents(__DIR__ . '/../fixtures/test.html');
+        
+        $testPage = file_get_contents($this->getPathFixture('test.html'));
+        
         Http::fake(Http::response($testPage));
         $response = $this
             ->followingRedirects()
