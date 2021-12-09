cdb454e143ab30bf19d2c5c9f20a94eb3b165b5f
Ev Sivkova <ev.sivkova@outlook.com>
2021-12-08 16:48:00 -0800

    smth

--- tests/Feature/CheckUrlTest.php
+++ tests/Feature/CheckUrlTest.php
@@ -20,24 +20,21 @@ class CheckUrlTest extends TestCase
 
     public function testCreateCheck(): void
     {
-        $domainName = "https://google.com";
-        $id = DB::table('urls')->insertGetId([
-            'name' => $domainName,
-        ]);
+        $domain = "https://google.com";
+        $id = DB::table('urls')->insertGetId(['name' => $domain]);
+
         $testPage = file_get_contents($this->getPathFixture('test.html'));
+
         Http::fake(Http::response($testPage));
-        $response = $this
-            ->followingRedirects()
-            ->post(route('checks.store', ['id' => $id]))
-            ->assertStatus(200);
+
+        $response = $this->followingRedirects()->post(route('checks.store', ['id' => $id]))->assertStatus(200);
 
         $response->assertSessionHasNoErrors();
-
         $this->assertDatabaseHas('url_checks', [
             'status_code' => 200,
             'h1' => 'test h1',
             'title' => 'test title',
-            'description' => 'test description',
+            'description' => 'test description'
         ]);
     }
 }
