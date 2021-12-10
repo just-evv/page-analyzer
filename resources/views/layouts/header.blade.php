18ee0100581a4a4c7934fe860f2071772fd0e1f7
Ev Sivkova <ev.sivkova@outlook.com>
2021-12-10 13:50:26 -0800

    upd front

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,19 @@
+
+<header class="flex-shrink-0">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">Analyzer</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active" href="{{ route('index') }}">Main</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link " href="{{ route('urls.all') }}">Added</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
