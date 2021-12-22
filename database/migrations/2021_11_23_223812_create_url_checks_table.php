e642c10ee9abcf796296fda5f1679430a0be6a81
Ev Sivkova <ev.sivkova@outlook.com>
2021-12-22 13:13:37 -0800

    upd description type

--- database/migrations/2021_11_23_223812_create_url_checks_table.php
+++ database/migrations/2021_11_23_223812_create_url_checks_table.php
@@ -19,7 +19,7 @@ class CreateUrlChecksTable extends Migration
             $table->foreignId('url_id');
             $table->integer('status_code')->nullable();           
             $table->string('title')->nullable();
-            $table->string('description')->nullable();
+            $table->text('description')->nullable();
             $table->string('h1')->nullable();
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
